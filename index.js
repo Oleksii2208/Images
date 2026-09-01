066a1ba49d717d76ae6a2367e168820e452bc030
Oleksii2208 <alekseyko2208@gmail.com>
2026-09-01 19:26:18 +0000

    Deploying to gh-pages from @ Oleksii2208/Images@d22b448aa0932203b403eee979222fd1031ec8c3 🚀

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-import{a as g,S as y,i as c}from"./assets/vendor-S2qh7U4E.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();async function u(o,t){const s="https://pixabay.com",n="/api/",e=new URLSearchParams({key:"56897880-ce564d0c5f2c9a99203aeddd2",q:o,page:t,per_page:15,image_type:"photo",orientation:"horizontal",safesearch:!0}),r=`${s}${n}?${e}`;return(await g.get(r)).data}const l={galleryList:document.querySelector(".gallery"),btnLoadMore:document.querySelector(".js-btn-load"),loader:document.querySelector(".loader")};function m(o){L();const t=o.map(h).join(`
-`);l.galleryList.insertAdjacentHTML("beforeend",t),new y(".gallery a",{captionsData:"alt",captionPosition:"botton",captionDelay:250,overlayOpacity:.7}).refresh()}function h(o){const{webformatURL:t,largeImageURL:s,tags:n,likes:e,views:r,comments:i,downloads:f}=o;return`<li class="gallery-item">
+import{a as L,S as b,i as u}from"./assets/vendor-S2qh7U4E.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();async function p(o,t,s){const i="https://pixabay.com",e="/api/",r=new URLSearchParams({key:"56897880-ce564d0c5f2c9a99203aeddd2",q:o,page:t,per_page:s,image_type:"photo",orientation:"horizontal",safesearch:!0}),n=`${i}${e}?${r}`;return(await L.get(n)).data}const l={galleryList:document.querySelector(".gallery"),btnLoadMore:document.querySelector(".btn-loadMore"),loader:document.querySelector(".loader")};function f(o){const t=o.map(q).join(`
+`);l.galleryList.insertAdjacentHTML("beforeend",t),new b(".gallery a",{captionsData:"alt",captionPosition:"botton",captionDelay:250,overlayOpacity:.7}).refresh()}function q(o){const{webformatURL:t,largeImageURL:s,tags:i,likes:e,views:r,comments:n,downloads:m}=o;return`<li class="gallery-item">
   <a class="gallery-link" href='${s}'>
     <img
       class="gallery-image"
       src='${t}'
       data-source='${s}'
-      alt='${n}'
+      alt='${i}'
       width='360'
       height='200'
     />
-    <div>
-    <p>Likes${e}</p>
-    <p>Views${r}</p>
-    <p>Comments${i}</p>
-    <p>Downloads${f}</p>
+    <div class='image-info'>
+    <p><strong>Likes</strong>${e}</p>
+    <p><strong>Views</strong>${r}</p>
+    <p><strong>Comments</strong>${n}</p>
+    <p><strong>Downloads</strong>${m}</p>
     </div>
   </a>
 </li>
-`}function L(){l.galleryList.innerHTML=""}function b(){l.loader.classList.remove("hidden")}function q(){l.loader.classList.add("hidden")}function v(){l.btnLoadMore.classList.remove("hidden")}function S(){l.btnLoadMore.classList.add("hidden")}const d={formElem:document.querySelector(".form"),btnLoadMore:document.querySelector(".js-btn-load"),galleryList:document.querySelector(".gallery")},a={query:"",page:1,total:100};d.formElem.addEventListener("submit",o=>{if(o.preventDefault(),a.query=o.currentTarget.elements.search.value.trim(),a.page=1,!a.query){c.error({title:"Error",message:"Please enter a search term!",position:"topRight"});return}b(),u(a.query,a.page).then(t=>{if(q(),t.hits.length===0){c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:1e4});return}m(t.hits),a.total=t.totalHits,p()}),d.formElem.reset()});d.btnLoadMore.addEventListener("click",()=>{a.page+=1,p(),u(a.query,a.page).then(o=>{m(o.hits)})});function p(){const t=Math.ceil(a.total/15);a.page>=t?(S(),c.info({title:"Info",message:"We're sorry, but you've reached the end of search results.",position:"topRight",timeout:1e4})):v()}
+`}function P(){l.galleryList.innerHTML=""}function h(){l.loader.classList.remove("hidden")}function c(){l.loader.classList.add("hidden")}function v(){l.btnLoadMore.classList.remove("hidden")}function g(){l.btnLoadMore.classList.add("hidden")}const d={formElem:document.querySelector(".form"),btnLoadMore:document.querySelector(".btn-loadMore"),galleryList:document.querySelector(".gallery"),targetElem:document.querySelector(".js-target")},a={query:"",page:1,total:100,perPage:15};//! Послуховувач події на форму, для збирання даних з поля введеня користувачем
+d.formElem.addEventListener("submit",o=>{if(o.preventDefault(),a.query=o.target.elements.search.value.trim(),a.page=1,P(),!a.query){u.error({title:"Error",message:"Please enter a search term!",position:"topRight"});return}h(),g(),p(a.query,a.page,a.perPage).then(t=>{c(),t.hits.length===0&&(u.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:1e4}),g()),f(t.hits),a.total=t.totalHits,y(),c()}),d.formElem.reset()});d.btnLoadMore.addEventListener("click",()=>{a.page+=1,y(),h(),p(a.query,a.page).then(o=>{f(o.hits),c()}),w()});function y(){const t=Math.ceil(a.total/15);a.page>=t?(g(),c(),u.info({title:"Info",message:"We're sorry, but you've reached the end of search results.",position:"topRight",timeout:1e4})):v()}//!=========== Роблю перевірку для observer
+//!============ Плавний скрол для рендеру нових картинок при Loadmore
+function w(){const t=d.galleryList.firstElementChild.getBoundingClientRect().height;window.scrollBy({behavior:"smooth",top:t*3})}
 //# sourceMappingURL=index.js.map
